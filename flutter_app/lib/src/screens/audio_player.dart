dd4144df088a889406167063554b0c9d1f5115ef
Maksim Nikolaev <maksim.nikolaev.dev@gmail.com>
2023-09-09 12:37:25 +0200

    update audio recorder

--- flutter_app/lib/src/screens/audio_player.dart
+++ flutter_app/lib/src/screens/audio_player.dart
@@ -1,265 +1,202 @@
-// This is a minimal example demonstrating a play/pause button and a seek bar.
-// More advanced examples demonstrating other features can be found in the same
-// directory as this example in the GitHub repository.
+import 'dart:async';
 
-import 'package:audio_session/audio_session.dart';
-import 'package:de_train/src/utils/ambiguate.dart';
-import 'package:de_train/src/utils/position_data.dart';
-import 'package:de_train/src/utils/show_dialog.dart';
-import 'package:de_train/src/widgets/seekbar.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:rxdart/rxdart.dart';
 
 class AudioPlayerWidget extends StatefulWidget {
+  /// Path from where to play recorded audio
+  final String source;
+
+  /// Callback when audio file should be removed
+  /// Setting this to null hides the delete button
+  final VoidCallback onDelete;
+
   const AudioPlayerWidget({
     Key? key,
-    required this.path,
-    required this.onBack,
-    required this.duration,
+    required this.source,
+    required this.onDelete,
   }) : super(key: key);
-
-  final String path;
-  final void Function() onBack;
-  final Duration duration;
 
   @override
   AudioPlayerWidgetState createState() => AudioPlayerWidgetState();
 }
 
-class AudioPlayerWidgetState extends State<AudioPlayerWidget>
-    with WidgetsBindingObserver {
-  final _player = AudioPlayer();
+class AudioPlayerWidgetState extends State<AudioPlayerWidget> {
+  static const double _controlSize = 56;
+  static const double _deleteBtnSize = 24;
+
+  late final AudioPlayer _audioPlayer;
+  Duration? _position;
+  Duration? _duration;
+
+  late Source source;
+
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    ambiguate(WidgetsBinding.instance)!.addObserver(this);
 
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.black,
-    ));
-
-    _init();
-  }
-
-  Future<void> _init() async {
-    // Inform the operating system of our app's audio attributes etc.
-    // We pick a reasonable default for an app that plays speech.
-    final session = await AudioSession.instance;
-    await session.configure(const AudioSessionConfiguration.speech());
-
-    // Listen to errors during playback.
-    _player.playbackEventStream.listen(
-      (event) {},
-      onError: (Object e, StackTrace stackTrace) {
-        if (kDebugMode) ('A stream error occurred: $e');
-      },
-    );
-
-    // Try to load audio from a source and catch any errors.
-    try {
-      await _player
-          .setAudioSource(
-        kIsWeb
-            ? AudioSource.uri(
-                Uri.parse(
-                  widget.path,
-                ),
-              )
-            : AudioSource.file(widget.path),
-      )
-          .then((value) {
-        if (kDebugMode) ("Audio source set! Duration: $value");
-      });
-    } catch (e) {
-      if (kDebugMode) print("Error loading audio source: $e");
-    }
+    init();
   }
 
   @override
   void dispose() {
-    ambiguate(WidgetsBinding.instance)!.removeObserver(this);
-    // Release decoders and buffers back to the operating system making them
-    // available for other apps to use.
-    _player.dispose();
+    _audioPlayer.dispose();
     super.dispose();
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.paused) {
-      // Release the player's resources when not in use. We use "stop" so that
-      // if the app resumes later, it will still remember what position to
-      // resume from.
-      _player.stop();
-    }
-  }
+  Future<void> init() async {
+    if (_isLoading) return;
 
-  /// Collects the data useful for displaying in a seek bar, using a handy
-  /// feature of rx_dart to combine the 3 streams of interest into one.
-  Stream<PositionData> get _positionDataStream =>
-      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
-        _player.positionStream,
-        _player.bufferedPositionStream,
-        _player.durationStream,
-        (position, bufferedPosition, duration) => PositionData(
-          position,
-          bufferedPosition,
-          duration ?? widget.duration,
-        ),
-      );
+    setState(() {
+      _isLoading = true;
+    });
+
+    source =
+        kIsWeb ? UrlSource(widget.source) : DeviceFileSource(widget.source);
+
+    _audioPlayer = AudioPlayer()..setReleaseMode(ReleaseMode.stop);
+
+    await _audioPlayer.setSource(source);
+
+    await _audioPlayer.getDuration().then(
+          (value) => setState(() {
+            if (kDebugMode) print('Duration: $value');
+            _duration = value;
+          }),
+        );
+
+    _audioPlayer.onPlayerComplete.listen((state) async {
+      await stop();
+      setState(() {});
+    });
+
+    _audioPlayer.onPositionChanged.listen(
+      (position) => setState(() {
+        _position = position;
+      }),
+    );
+
+    _audioPlayer.onDurationChanged.listen(
+      (duration) => setState(() {
+        _duration = duration;
+      }),
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Display play/pause button and volume/speed sliders.
-              ControlButtons(_player, widget.onBack),
-              // Display seek bar. Using StreamBuilder, this widget rebuilds
-              // each time the position, buffered position or duration changes.
-              StreamBuilder<PositionData>(
-                stream: _positionDataStream,
-                builder: (context, snapshot) {
-                  final positionData = snapshot.data;
-                  if (kDebugMode) {
-                    print(
-                      "Position data: ${positionData.toString()}",
-                    );
-                  }
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (_isLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-                  if (kDebugMode) print("Duration: ${widget.duration}");
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                _buildControl(),
+                _buildSlider(constraints.maxWidth),
+                IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Color(0xFF73748D),
+                    size: _deleteBtnSize,
+                  ),
+                  onPressed: () {
+                    if (_audioPlayer.state == PlayerState.playing) {
+                      stop().then((value) => widget.onDelete());
+                    } else {
+                      widget.onDelete();
+                    }
+                  },
+                ),
+              ],
+            ),
+            Text('${_duration ?? 0.0}'),
+          ],
+        );
+      },
+    );
+  }
 
-                  return SeekBar(
-                    duration: positionData?.duration ?? widget.duration,
-                    position: positionData?.position ?? Duration.zero,
-                    bufferedPosition:
-                        positionData?.bufferedPosition ?? Duration.zero,
-                    onChangeEnd: _player.seek,
-                  );
-                },
-              ),
-            ],
-          ),
+  Widget _buildControl() {
+    Icon icon;
+    Color color;
+
+    if (_audioPlayer.state == PlayerState.playing) {
+      icon = const Icon(Icons.pause, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = Icon(Icons.play_arrow, color: theme.primaryColor, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child:
+              SizedBox(width: _controlSize, height: _controlSize, child: icon),
+          onTap: () {
+            if (_audioPlayer.state == PlayerState.playing) {
+              pause();
+            } else {
+              play();
+            }
+          },
         ),
       ),
     );
   }
-}
 
-/// Displays the play/pause button and volume/speed sliders.
-class ControlButtons extends StatelessWidget {
-  final AudioPlayer player;
-  final void Function() onBack;
+  Widget _buildSlider(double widgetWidth) {
+    bool canSetValue = false;
+    final duration = _duration;
+    final position = _position;
 
-  const ControlButtons(
-    this.player,
-    this.onBack, {
-    Key? key,
-  }) : super(key: key);
+    if (duration != null && position != null) {
+      canSetValue = position.inMilliseconds > 0;
+      canSetValue &= position.inMilliseconds < duration.inMilliseconds;
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        // Opens volume slider dialog
-        IconButton(
-          icon: const Icon(Icons.volume_up),
-          onPressed: () {
-            showSliderDialog(
-              context: context,
-              title: "Adjust volume",
-              divisions: 10,
-              min: 0.0,
-              max: 1.0,
-              value: player.volume,
-              stream: player.volumeStream,
-              onChanged: player.setVolume,
-            );
-          },
-        ),
+    double width = widgetWidth - _controlSize - _deleteBtnSize;
+    width -= _deleteBtnSize;
 
-        /// This StreamBuilder rebuilds whenever the player state changes, which
-        /// includes the playing/paused state and also the
-        /// loading/buffering/ready state. Depending on the state we show the
-        /// appropriate button or loading indicator.
-        StreamBuilder<PlayerState>(
-          stream: player.playerStateStream,
-          builder: (context, snapshot) {
-            final playerState = snapshot.data;
-            final processingState = playerState?.processingState;
-            final playing = playerState?.playing;
-            if (processingState == ProcessingState.loading ||
-                processingState == ProcessingState.buffering) {
-              return Container(
-                margin: const EdgeInsets.all(8.0),
-                width: 64.0,
-                height: 64.0,
-                child: const CircularProgressIndicator(),
-              );
-            } else if (playing != true) {
-              return IconButton(
-                icon: const Icon(Icons.play_arrow),
-                iconSize: 64.0,
-                onPressed: player.play,
-              );
-            } else if (processingState != ProcessingState.completed) {
-              return IconButton(
-                icon: const Icon(Icons.pause),
-                iconSize: 64.0,
-                onPressed: player.pause,
-              );
-            } else {
-              return IconButton(
-                icon: const Icon(Icons.replay),
-                iconSize: 64.0,
-                onPressed: () => player.seek(Duration.zero),
-              );
-            }
-          },
-        ),
-
-        // Stop and go back to recording audio
-        // Opens volume slider dialog
-        IconButton(
-          icon: const Icon(Icons.stop),
-          onPressed: onBack,
-        ),
-
-        // Opens speed slider dialog
-        StreamBuilder<double>(
-          stream: player.speedStream,
-          builder: (context, snapshot) => IconButton(
-            icon: Text(
-              "${snapshot.data?.toStringAsFixed(1)}x",
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            onPressed: () {
-              showSliderDialog(
-                context: context,
-                title: "Adjust speed",
-                divisions: 10,
-                min: 0.5,
-                max: 1.5,
-                value: player.speed,
-                stream: player.speedStream,
-                onChanged: player.setSpeed,
-              );
-            },
-          ),
-        ),
-      ],
+    return SizedBox(
+      width: width,
+      child: Slider(
+        activeColor: Theme.of(context).primaryColor,
+        inactiveColor: Theme.of(context).colorScheme.secondary,
+        onChanged: (v) {
+          if (duration != null) {
+            final position = v * duration.inMilliseconds;
+            _audioPlayer.seek(Duration(milliseconds: position.round()));
+          }
+        },
+        value: canSetValue && duration != null && position != null
+            ? position.inMilliseconds / duration.inMilliseconds
+            : 0.0,
+      ),
     );
   }
+
+  Future<void> play() {
+    return _audioPlayer.play(source);
+  }
+
+  Future<void> pause() => _audioPlayer.pause();
+
+  Future<void> stop() => _audioPlayer.stop();
 }
--- flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import audio_session
-import just_audio
+import audioplayers_darwin
 import path_provider_foundation
 import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacosPlugin.register(with: registry.registrar(forPlugin: "RecordMacosPlugin"))
 }
